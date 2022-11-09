4ca10a7685ecbfcb56a5b754a05124ab6e0fa24c
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2022-11-09 21:01:43 +0700

    Create chan 3.cpp

---
+++ chan 3.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void checkNumber(int n,int m){
+	int i;
+	if(n<m){
+		for(i=n+1;i<m;i++){
+			if(i%2==0)
+			printf("\n%d",i);
+			
+		}
+	}
+	if(m<n){
+		for(i=m+1;i<n;i++){
+			if(i%2==0)
+			printf("\n%d",i);
+		}
+	}
+}
+	int main(){
+		int i,m,n;
+		printf("Nhap hai so m,n: ");
+		scanf("%d%d", &m,&n);
+		checkNumber(m,n);
+		return 0;
+	
+}
