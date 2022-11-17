c66feb5c60559c842cdf10363392d386c9bbe050
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2022-11-17 15:13:22 +0700

    Create tim so chan.cpp

---
+++ tim so chan.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void checkNumber(int n){
+	if(n % 2 ==0){
+		printf("1");
+		
+	}
+	else{
+		printf("0");
+		
+	}
+}
+int main(){
+    int n;
+	printf("\Nhap n: ");
+	scanf("%d", &n);
+	checkNumber(n);
+	return 0;	
+}
